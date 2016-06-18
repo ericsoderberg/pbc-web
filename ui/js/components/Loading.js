e01c0bc0fe0dcc5ae171f69fbbc42764475c041e
Eric Soderberg <esoderi@gmail.com>
2016-06-18 07:20:32 -0700

    Animate Loading. Stagger page section animation.

--- ui/js/components/Loading.js
+++ ui/js/components/Loading.js
@@ -3,10 +3,18 @@ import React from 'react';
 import Section from './Section';
 
 const Loading = (props) => {
+  const dots = [];
+  for (let i=0; i<=3; i++) {
+    dots.push(
+      <svg key={i} viewbox="0 0 24 24" width="24" height="24">
+        <circle stroke="none" cx="12" cy="12" r="12" />
+      </svg>
+    );
+  }
   return (
     <Section full={true}>
       <div className="loading">
-        Loading ...
+        {dots}
       </div>
     </Section>
   );
