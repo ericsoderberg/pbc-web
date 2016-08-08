64b46d312eb9d022552758895555e6c42c4b5bb6
Eric Soderberg <esoderi@gmail.com>
2016-08-08 10:11:07 -0700

    Improve ConfirmRemove styling when mobile.

--- ui/js/components/ConfirmRemove.js
+++ ui/js/components/ConfirmRemove.js
@@ -29,7 +29,8 @@ export default class ConfirmRemove extends Component {
     return (
       <div className={classNames.join(' ')}>
         <div className="confirm-remove__confirm">
-          <button className="button" onClick={this.props.onConfirm}>Confirm</button>
+          <button className="button button--secondary"
+            onClick={this.props.onConfirm}>Confirm</button>
           <button className="button" onClick={this._onCancel}>Cancel</button>
         </div>
         <button className="button button--secondary confirm-remove__remove"
