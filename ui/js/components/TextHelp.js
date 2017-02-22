fe0dd59c16cc69d2d03f174124d29b378485be82
Eric Soderberg <esoderi@gmail.com>
2017-02-21 20:40:41 -0800

    Inline Markdown syntax help.

--- ui/js/components/TextHelp.js
+++ ui/js/components/TextHelp.js
@@ -1,9 +1,39 @@
 "use strict";
-import React from 'react';
+import React, { Component } from 'react';
 
-const TextHelp = (props) => (
-  <a href="http://daringfireball.net/projects/markdown/syntax"
-    target="_blank">Markdown syntax</a>
-);
+const HELP =
+`# Heading
+## Heading
+* list item
+*italic*
+**bold**
+[link label](link url)
+> quote
+`;
 
-export default TextHelp;
+export default class TextHelp extends Component {
+
+  constructor () {
+    super();
+    this._onToggle = this._onToggle.bind(this);
+    this.state = { active: false };
+  }
+
+  _onToggle (event) {
+    this.setState({ active: ! this.state.active });
+  }
+
+  render () {
+    const { active } = this.state;
+    let classNames = ['text-help'];
+    if (active) {
+      classNames.push('text-help--active');
+    }
+    return (
+      <div className={classNames.join(' ')}>
+        <span onClick={this._onToggle}>syntax</span>
+        <pre className='text-help__drop'>{HELP}</pre>
+      </div>
+    );
+  }
+};
