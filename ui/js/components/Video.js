54096fa06e21d710186c40cf5e6824024e0f46ef
Eric Soderberg <esoderi@gmail.com>
2017-03-04 15:05:21 -0800

    Added autoplay to Video.

--- ui/js/components/Video.js
+++ ui/js/components/Video.js
@@ -6,15 +6,18 @@ const YOUTUBE_REGEXP = /youtube\.com\/(\w+)|youtu\.be\/(\w+)/;
 export default class Video extends Component {
 
   render() {
-    const { url } = this.props;
+    const { autoplay, url } = this.props;
 
     let contents = <span>Unknown type</span>;
     if (url) {
       // TOOD: Handle YouTube vs. Vimeo
       let match = url.match(VIMEO_REGEXP);
       if (match) {
-        const src = `${window.location.protocol}//player.vimeo.com/video/` +
+        let src = `${window.location.protocol}//player.vimeo.com/video/` +
           `${match[1]}?title=0&byline=0&portrait=0`;
+        if (autoplay) {
+          src += '&autoplay=1';
+        }
         contents = (
           <iframe className="video vimeo-player" type="text/html"
             width="960" height="540"
@@ -38,9 +41,11 @@ export default class Video extends Component {
 }
 
 Video.propTypes = {
+  autoplay: PropTypes.bool,
   url: PropTypes.string,
 };
 
 Video.defaultProps = {
+  autoplay: true,
   url: undefined,
 };
