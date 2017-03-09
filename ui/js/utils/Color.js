fa0d1d2b9577e991001ee6bee6d2a95cfb047a5d
Eric Soderberg <esoderi@gmail.com>
2017-03-08 17:31:45 -0800

    Fixed some issues with dark background detection.

--- ui/js/utils/Color.js
+++ ui/js/utils/Color.js
@@ -1,12 +1,17 @@
 
-const COLOR_RGB_REGEXP = /#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})/;
+const COLOR_HASH_SHORT_REGEXP = /#([0-9a-fA-F]{1})([0-9a-fA-F]{1})([0-9a-fA-F]{1})/;
+const COLOR_HASH_REGEXP = /#([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})/;
+const COLOR_RGB_REGEXP = /rgb\((\d+), (\d+), (\d+)\)/;
+const COLOR_RGBA_REGEXP = /rgba\((\d+), (\d+), (\d+), ([\d.]+)\)/;
 
-export function isDarkBackground(colorString) {
+export function isDarkBackground(color) {
   // convert to RGB elements
-  const match = colorString.match(COLOR_RGB_REGEXP);
+  const match = color.match(COLOR_RGB_REGEXP) ||
+    color.match(COLOR_RGBA_REGEXP) || color.match(COLOR_HASH_REGEXP) ||
+    color.match(COLOR_HASH_SHORT_REGEXP);
   let result = false;
   if (match) {
-    const [red, green, blue] = match.slice(1).map(n => parseInt(n, 10));
+    const [red, green, blue] = match.slice(1).map(n => parseInt(n, 16));
     // http://www.had2know.com/technology/
     //  color-contrast-calculator-web-design.html
     const brightness = (
