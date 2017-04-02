62eb9492717119a9d85845c654a42c149db41fca
Eric Soderberg <eric.soderberg@yahoo.com>
2017-04-02 13:01:13 -0700

    Fixed an issue with MessageAdd.

--- ui/js/pages/message/MessageAdd.js
+++ ui/js/pages/message/MessageAdd.js
@@ -1,11 +1,13 @@
 
 import React, { PropTypes } from 'react';
 import Add from '../../components/Add';
+import { searchToObject } from '../../utils/Params';
 import MessageFormContents from './MessageFormContents';
 import MessagePreview from './MessagePreview';
 
 const MessageAdd = (props) => {
-  const { location: { query } } = props;
+  const { location } = props;
+  const query = searchToObject(location.search);
   return (
     <Add category="messages" FormContents={MessageFormContents}
       Preview={MessagePreview} showable={true} title="Add Message"
@@ -14,11 +16,7 @@ const MessageAdd = (props) => {
 };
 
 MessageAdd.propTypes = {
-  location: PropTypes.shape({
-    query: PropTypes.shape({
-      libraryId: PropTypes.string,
-    }),
-  }).isRequired,
+  location: PropTypes.object.isRequired,
 };
 
 export default MessageAdd;
