f711d79fb7235929814c0d96109fc696612bc5b5
Eric Soderberg <eric.soderberg@yahoo.com>
2017-09-05 12:17:50 -0700

    Tweaked event searching

--- server/api/search.js
+++ server/api/search.js
@@ -10,92 +10,92 @@ mongoose.Promise = global.Promise;
 export default function (router) {
   router.get('/search', (req, res) => {
     getSession(req)
-    .then((session) => {
-      // start with pages
-      const Page = mongoose.model('Page');
-      return Page.find(
-        {
-          $text: { $search: req.query.search },
-          public: true,
-        },
-        { score: { $meta: 'textScore' } },
-      )
-      .sort({ score: { $meta: 'textScore' }, modified: -1 })
-      .limit(10)
-      .exec()
-      .then(pages => ({ session, pages }));
-    })
-    .then((context) => {
-      let { pages } = context;
+      .then((session) => {
+        // start with pages
+        const Page = mongoose.model('Page');
+        return Page.find(
+          {
+            $text: { $search: req.query.search },
+            public: true,
+          },
+          { score: { $meta: 'textScore' } },
+        )
+          .sort({ score: { $meta: 'textScore' }, modified: -1 })
+          .limit(10)
+          .exec()
+          .then(pages => ({ session, pages }));
+      })
+      .then((context) => {
+        let { pages } = context;
 
-      // prune sections down to just text
-      const exp = new RegExp(req.query.search, 'i');
-      pages = pages.map((doc) => {
-        doc.sections = doc.sections
-        .filter(section => section.type === 'text' && section.text)
-        .map((section) => {
-          // prune text down to just snippet with first match
-          const index = section.text.search(exp);
-          const fromIndex =
-            Math.max(0, section.text.lastIndexOf(' ', Math.max(0, index - 80)));
-          const toIndex = section.text.indexOf(' ', index + 80);
+        // prune sections down to just text
+        const exp = new RegExp(req.query.search, 'i');
+        pages = pages.map((doc) => {
+          doc.sections = doc.sections
+            .filter(section => section.type === 'text' && section.text)
+            .map((section) => {
+              // prune text down to just snippet with first match
+              const index = section.text.search(exp);
+              const fromIndex =
+                Math.max(0, section.text.lastIndexOf(' ', Math.max(0, index - 80)));
+              const toIndex = section.text.indexOf(' ', index + 80);
 
-          section.text = section.text
-          .slice(fromIndex, toIndex)
-          .replace(exp, `**${req.query.search}**`);
+              section.text = section.text
+                .slice(fromIndex, toIndex)
+                .replace(exp, `**${req.query.search}**`);
 
-          return section;
+              return section;
+            });
+
+          // Add at least one blank text section
+          if (doc.sections.length === 0) {
+            doc.sections.push({ type: 'text', text: '' });
+          }
+
+          return doc;
         });
 
-        // Add at least one blank text section
-        if (doc.sections.length === 0) {
-          doc.sections.push({ type: 'text', text: '' });
-        }
-
-        return doc;
-      });
-
-      return { ...context, pages };
-    })
-    // check events too
-    .then((context) => {
-      const Event = mongoose.model('Event');
-      const recently = moment().subtract(1, 'week');
-      const dateCriteria = [
-        { end: { $gte: recently.toDate() } },
-        { dates: { $gte: recently.toDate() } },
-      ];
-      return Event.find(
-        {
-          $text: { $search: req.query.search },
-          public: true,
-          $or: dateCriteria,
-        },
-        { score: { $meta: 'textScore' } },
-      )
-      .sort({ score: { $meta: 'textScore' }, modified: -1 })
-      .limit(10)
-      .exec()
-      .then(events => ({ ...context, events }));
-    })
-    // check libraries
-    .then((context) => {
-      const Library = mongoose.model('Library');
-      return Library.find(
-        {
-          $text: { $search: req.query.search },
-        },
-        { score: { $meta: 'textScore' } },
-      )
-      .sort({ score: { $meta: 'textScore' }, modified: -1 })
-      .limit(10)
-      .exec()
-      .then(libraries => ({ ...context, libraries }));
-    })
-    .then((context) => {
-      const { pages, events, libraries } = context;
-      res.status(200).json({ pages, events, libraries });
-    })
-    .catch(error => catcher(error, res));
+        return { ...context, pages };
+      })
+      // check events too
+      .then((context) => {
+        const Event = mongoose.model('Event');
+        // const recently = moment().subtract(1, 'week');
+        // const dateCriteria = [
+        //   { end: { $gte: recently.toDate() } },
+        //   { dates: { $gte: recently.toDate() } },
+        // ];
+        return Event.find(
+          {
+            $text: { $search: req.query.search },
+            public: true,
+            // $or: dateCriteria,
+          },
+          { score: { $meta: 'textScore' } },
+        )
+          .sort({ score: { $meta: 'textScore' }, modified: -1 })
+          .limit(10)
+          .exec()
+          .then(events => ({ ...context, events }));
+      })
+      // check libraries
+      .then((context) => {
+        const Library = mongoose.model('Library');
+        return Library.find(
+          {
+            $text: { $search: req.query.search },
+          },
+          { score: { $meta: 'textScore' } },
+        )
+          .sort({ score: { $meta: 'textScore' }, modified: -1 })
+          .limit(10)
+          .exec()
+          .then(libraries => ({ ...context, libraries }));
+      })
+      .then((context) => {
+        const { pages, events, libraries } = context;
+        res.status(200).json({ pages, events, libraries });
+      })
+      .catch(error => catcher(error, res));
   });
 }
